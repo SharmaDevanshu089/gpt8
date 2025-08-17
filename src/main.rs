58bf4e282d272fb9bbbd1c51f198421db867a583
SharmaDevanshu089 <SharmaDevanshu089@gmail.com>
2025-08-17 23:07:26 +0530

    Removed Fricking jetbrains

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ fn main() {
     let integer_to_be_passed:i32= 5;
     increment_in_place(integer_to_be_passed);
 }
-fn increment_in_place(integer_that_is_passed:s i32){
+fn increment_in_place(integer_that_is_passed: i32){
     print!("{}", integer_that_is_passed);
 }
